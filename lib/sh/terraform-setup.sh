b703db570266e17e1921bb61c4afae35b0ee44d8
James Lott <james.l@jarv.us>
2022-08-11 12:01:11 -0400

    engine: refactor tool integration points into single component

---
+++ lib/sh/terraform-setup.sh
@@ -0,0 +1,17 @@
+required_missing=()
+
+test "$ENGINE_ROOT"           || { printf 'FATAL BUG: ENGINE_ROOT is not defined!\n'  >&2; exit 1; }
+test "$CONTENT_ROOT"          || { printf 'FATAL BUG: CONTENT_ROOT is not defined!\n' >&2; exit 1; }
+test "$INFRA_DEPLOYMENT_NAME" || required_missing+=('INFRA_DEPLOYMENT_NAME')
+test "$INFRA_TARGET_NAME"     || required_missing+=('INFRA_TARGET_NAME')
+
+. "$ENGINE_ROOT"/lib/sh/validate-required.sh
+
+INFRA_DEPLOYMENT_ENDPOINT=$CONTENT_ROOT/deployments/terraform/$INFRA_DEPLOYMENT_NAME
+INFRA_TARGET_ENDPOINT=$CONTENT_ROOT/targets/$INFRA_TARGET_NAME/terraform/$INFRA_DEPLOYMENT_NAME
+
+TERRAFORM=('terraform' "-chdir=$INFRA_DEPLOYMENT_ENDPOINT")
+
+if [[ ! -e $INFRA_DEPLOYMENT_ENDPOINT/.terraform ]]; then
+  "${TERRAFORM[@]}" init -backend-config="$INFRA_TARGET_ENDPOINT/target.tfbackend"
+fi
