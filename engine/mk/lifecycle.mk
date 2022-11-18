e15f2c19b32a8bc5c460418afeaee4bfa35fdcbd
James Lott <james@lottspot.com>
2022-11-18 12:52:57 -0500

    mk: create lifecycle orchestration constructs

---
+++ engine/mk/lifecycle.mk
@@ -0,0 +1,16 @@
+include $(dir $(lastword $(MAKEFILE_LIST)))/engine.mk
+
+ifeq ($(LIFECYCLE_NAME),)
+LIFECYCLE_NAME := $(shell basename "`realpath .`")
+endif
+
+ifeq ($(LIFECYCLE_DIR),)
+LIFECYCLE_DIR  := $(LIFECYCLE_HOME)/$(LIFECYCLE_NAME)
+endif
+
+ifeq ($(ENGINE_STAGES),)
+ENGINE_STAGES  := $(shell find . -mindepth 1 -maxdepth 1 -type d ! -name '.*' -exec basename {} \;)
+endif
+
+export LIFECYCLE_NAME
+export LIFECYCLE_DIR
