9277f74838417ec8fe9b66c2be2e603e07d5f254
James Lott <james@lottspot.com>
2022-11-18 19:56:30 -0500

    subject.mk: add default target and config module

--- engine/mk/subject.mk
+++ engine/mk/subject.mk
@@ -11,3 +11,12 @@ endif
 ifeq ($(LIFECYCLE_NAME),)
 LIFECYCLE_NAME := $(shell basename "`realpath ../..`")
 endif
+
+help:
+	@echo
+	@echo Activities:
+	@echo
+	@echo     To view, open: $(shell realpath .)/Makefile
+	@echo
+
+include $(MK)/config.mk
