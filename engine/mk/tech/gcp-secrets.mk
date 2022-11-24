163ed2f8bddd90345c22f32e694ef7c5a00060c9
James Lott <james@lottspot.com>
2022-11-24 09:02:12 -0500

    gcp-secrets.mk: refactor for gcpSecret config object
    This allows explicit source id/destination file definitions in engine
    config rather than performing opaque magic based on filename
    conventions.

--- engine/mk/tech/gcp-secrets.mk
+++ engine/mk/tech/gcp-secrets.mk
@@ -1,35 +1,50 @@
+#
+# Config:
+#
+# [env "<env-name>"]
+# gcpSecret = foo-secret
+# gcpSecret = bar-secret
+# [gcpSecret "foo-secret"]
+# id   = <secret-id>
+# file = <env-subpath>
+# [gcpSecret "bar-secret"]
+# ...
+#
+
 include $(MK)/tech/gcloud.mk
 
-GCP_SECRETS_PATHS          ?= $(foreach subpath,$(call env_config,gcpSecretFile,--get-all),$(call env_pathjoin,$(subpath)))
-
-ifeq ($(GCP_SECRETS_IDS),)
-GCP_SECRETS_IDS            := $(foreach path,$(GCP_SECRETS_PATHS),$(basename $(notdir $(path))))
-endif
-
-GCP_SECRETS_CREATE_TARGETS := $(patsubst %,gcp-secrets-create-%,$(GCP_SECRETS_IDS))
-GCP_SECRETS_UPDATE_TARGETS := $(patsubst %,gcp-secrets-upload-%,$(GCP_SECRETS_IDS))
+GCP_SECRETS_NAMES          ?= $(call env_config,gcpSecret,--get-all)
+GCP_SECRETS_PATHS          ?= $(foreach name,$(GCP_SECRETS_NAMES),$(call gcpSecret_path,$(name)))
 
 gcp-secrets-create:
 gcp-secrets-upload:
 gcp-secrets-get:
 
-gcpSecretFile_get_id        = $(basename $(notdir $(1)))
-gcpSecretFile_id_valpattern = (^|/)$(1)(\.[^.]+)?$$
+GCP_SECRETS_CREATE_TARGETS := $(patsubst %,gcp-secrets-create-%,$(GCP_SECRETS_NAMES))
+GCP_SECRETS_UPDATE_TARGETS := $(patsubst %,gcp-secrets-upload-%,$(GCP_SECRETS_NAMES))
+
+gcpSecret_id            = $(shell $(GIT_CONFIG) gcpSecret.$(1).id)
+gcpSecret_path          = $(call env_pathjoin,$(shell $(GIT_CONFIG) gcpSecret.$(1).file))
+gcpSecret_name_fromfile = $(shell $(GIT_CONFIG) --get-regex 'gcpSecret\.[^.]+\.file' '(^|\./)?$(1)$$' | cut -d. -f2)
 
 ifneq ($(GCP_SECRETS_CREATE_TARGETS),)
 gcp-secrets-create: $(GCP_SECRETS_CREATE_TARGETS)
 gcp-secrets-create-%:
-	$(if $(shell $(GCLOUD) secrets versions describe latest --secret='$*' 2>/dev/null),,$(GCLOUD) secrets create '$*' --replication-policy=automatic)
+	$(if $(shell $(GCLOUD) secrets versions describe latest --secret='$(call gcpSecret_id,$*)' 2>/dev/null),,$(GCLOUD) secrets create '$(call gcpSecret_id,$*)' --replication-policy=automatic)
 endif
 
 ifneq ($(GCP_SECRETS_UPDATE_TARGETS),)
 gcp-secrets-upload: $(GCP_SECRETS_UPDATE_TARGETS)
 gcp-secrets-upload-%: gcp-secrets-create
-	$(GCLOUD) secrets versions add '$*' --data-file='$(call env_pathjoin,$(call env_config,gcpSecretFile '$(call gcpSecretFile_id_valpattern,$*)',--get))'
+	$(GCLOUD) secrets versions add '$*' --data-file='$(call gcpSecret_path,$*)'
 endif
 
 ifneq ($(GCP_SECRETS_PATHS),)
 gcp-secrets-get: $(GCP_SECRETS_PATHS)
 $(GCP_SECRETS_PATHS):
-	$(GCLOUD) secrets versions access latest --secret='$(call gcpSecretFile_get_id,$@)' --out-file='$@'
+	$(GCLOUD) secrets versions access latest --secret='$(call gcpSecret_id,$(call gcpSecret_name_fromfile,$(call env_pathstrip,$@)))' --out-file='$@'
 endif
+
+.PHONY: gcp-secrets-create
+.PHONY: gcp-secrets-upload
+.PHONY: gcp-secrets-get
