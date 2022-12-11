8c05e6e53895d7dec94f690875f4c91ecfc02778
James Lott <james@lottspot.com>
2022-12-11 16:52:28 -0500

    engine: test.mk, test/virsh, test/ssh: refactor for test_skip_{subject|env}
    This allows the skip message to vary slightly depending on whether the
    missing config entry is related to the subject or the environment.

--- mk/test.mk
+++ mk/test.mk
@@ -1,1 +1,2 @@
-test_skip = $(info SKIP: $(LIFECYCLE_NAME)/$(SUBJECT_NAME): $(1) not configured)
+test_skip_subject = $(info SKIP: $(LIFECYCLE_NAME)/$(SUBJECT_NAME): $(1) not configured)
+test_skip_env     = $(info SKIP: environment $(ENGINE_ENV): $(1) not configured)
