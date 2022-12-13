79152b68dfe649a58f8112776406d49321786d1d
James Lott <james.l@jarv.us>
2022-12-13 12:48:05 -0500

    engine: stage.mk: change default behavior to run "stage" target
    This is a UX improvement which allows a stage to more easily be run
    using make -C <stage-dir>.

--- mk/stage.mk
+++ mk/stage.mk
@@ -19,11 +19,14 @@ export STAGE_NAME
 export PIPELINE_NAME
 export DOING_ALL
 
+DEFAULT_TARGET  := stage
+EXTRA_TARGETS   := $(filter-out stage,$(MAKECMDGOALS))
 SUBJECT_TARGETS := $(patsubst %,subject-%,$(DO_SUBJECTS))
 
 subject_exists = $(shell find . -mindepth 1 -maxdepth 1 -type d -name $(1) 2>/dev/null)
 
-$(MAKECMDGOALS): $(SUBJECT_TARGETS)
+# this ensures that if no other targets are passed, "stage" will run
+$(DEFAULT_TARGET) $(EXTRA_TARGETS): $(SUBJECT_TARGETS)
 
 subject-%:
 	$(if $(call subject_exists,$*),$(MAKE) -C '$*' $(MAKECMDGOALS))
