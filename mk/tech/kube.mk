a44c9a5b8b0fb344bf3dff477a730f793878a36d
James Lott <james.l@jarv.us>
2022-12-13 11:32:05 -0500

    engine: kube.mk: implement tech module

--- mk/tech/kube.mk
+++ mk/tech/kube.mk
@@ -1,1 +1,11 @@
-KUBECONFIG = $(error NOT IMPLEMENTED)
+#
+# Config:
+#
+# [env "<name>"]
+# kubeconfig = <absolute-path>
+KUBECONFIG ?= $(call env_config,kubeconfig)
+KUBECTL    ?= KUBECONFIG='$(KUBECONFIG)' kubectl
+
+kube_ns_exists = $(if $(shell $(KUBECTL) get ns '$(1)' 2>/dev/null),1)
+
+export KUBECONFIG
