4e179b2f27a95002d03bc27068bcead9d19b5ecf
James Lott <james.l@jarv.us>
2022-12-22 11:19:38 -0500

    engine: document core modules

--- engine/mk/subject.mk
+++ engine/mk/subject.mk
@@ -1,4 +1,30 @@
+# = subject =
+#
+# Provider of subject-local context and methods
+#
+# == Context ==
+#
+# |===================================
+# | Name                 | Description
+# | SUBJECT_DIR          | Absolute path to the directory containing the Subject file (<pipeline>/<stage>/<subject>/Makefile)
+# | SUBJECT_NAME         | Name of the currently in-scope subject
+# | STAGE_NAME           | Name of the currently in-scope stage
+# | PIPELINE_NAME        | Name of the currently in-scope pipeline
+# |===================================
+#
+# == Methods ==
+#
+# `run_step`::
+#   positionals:::
+#     1. step name
+#   returns:::
+#     * shell command to embed in a target recipe
+
 include $(dir $(lastword $(MAKEFILE_LIST)))/engine.mk
+
+ifeq ($(SUBJECT_DIR),)
+SUBJECT_DIR   := $(shell realpath .)
+endif
 
 ifeq ($(SUBJECT_NAME),)
 SUBJECT_NAME  := $(shell basename "`realpath .`")
@@ -10,10 +36,6 @@ endif
 
 ifeq ($(PIPELINE_NAME),)
 PIPELINE_NAME := $(shell basename "`realpath ../..`")
-endif
-
-ifeq ($(SUBJECT_DIR),)
-SUBJECT_DIR   := $(shell realpath .)
 endif
 
 run_step = $(MAKE) $(1)
