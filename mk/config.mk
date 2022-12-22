168141fda0c4c705aa3593e4fc0fae8baa262635
James Lott <james.l@jarv.us>
2022-12-22 13:37:54 -0500

    engine: config.mk: rename artifact_match to artifacts_matching
    This makes more explicit the multi-name nature of the return value

--- mk/config.mk
+++ mk/config.mk
@@ -50,11 +50,11 @@
 #     * <ENGINE_ARTIFACTS_DIR>/<engineArtifact-path-value>
 #     * blank when <artifact-name> does not exist or does not define a "path" variable
 #
-# `artifact_match`::
+# `artifacts_matching`::
 #   positionals:::
 #     1. artifact pattern. Anchors (^ and $) will break matching and must not be used.
 #   returns:::
-#     * All artifact names which match <artifact-pattern> and define a "path" variable
+#     * Names of artifacts which match <artifact-pattern> and define a "path" variable
 #
 # `artifacts_using_producer`::
 #   positionals:::
@@ -95,5 +95,5 @@ subject_config_path      = $(if $(call subject_config,$(1)),$(call project_pathj
 
 artifact_var             = $(shell $(GIT_CONFIG) $(3) engineArtifact.$(1).$(2))
 artifact_path            = $(if $(call artifact_var,$(1),path),$(call artifact_pathjoin,$(call artifact_var,$(1),path)))
-artifact_match           = $(shell $(GIT_CONFIG) --get-regexp '^engineArtifact\.$(1)\.path$$' | cut -d. -f2)
+artifacts_matching       = $(shell $(GIT_CONFIG) --get-regexp '^engineArtifact\.$(1)\.path$$' | cut -d. -f2)
 artifacts_using_producer = $(shell $(GIT_CONFIG) --get-regexp '^engineArtifact\.[^.]+\.producer$$' '^$(1)$$' | cut -d. -f2)
