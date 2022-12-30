bd9ad1bf105d230a57c67fb3188c75aa23a9fa7a
James Lott <james.l@jarv.us>
2022-12-30 10:18:40 -0500

    engine: helm.mk: fix deployment of releases without namespaces

--- mk/tech/helm.mk
+++ mk/tech/helm.mk
@@ -116,12 +116,12 @@ HELM             += $(HELM_ARGS)
 helm_release_exists        = $(shell if $(HELM) status '$(HELM_RELEASE)' &>/dev/null; then echo 1; else false; fi)
 helm_release_changed       = $(or $(filter 1,$(helm_diff_status)),$(if $(helm_diff_is_error),$(info NOTICE: helm_diff_status encountered an error in stage $(PIPELINE_NAME)/$(STAGE_NAME) for subject $(SUBJECT_NAME); deployment will be unchanged)))
 helm_dependencies_drifted  = $(shell $(HELM) dependency list '$(HELM_CHART)' | tail -n+2 | grep -v 'ok[[:space:]]*$$' | awk '{print $$1}')
-helm_diff_status          := $(if $(helm_release_exists),$(shell $(HELM) template '$(HELM_RELEASE)' '$(HELM_CHART)' $(HELM_VALUES_ARGS) | $(KUBECTL) diff --namespace '$(HELM_NAMESPACE)' -f - >/dev/null; echo $$?))
+helm_diff_status          := $(if $(helm_release_exists),$(shell $(HELM) template '$(HELM_RELEASE)' '$(HELM_CHART)' $(HELM_VALUES_ARGS) | $(KUBECTL) diff $(if $(HELM_NAMESPACE),--namespace '$(HELM_NAMESPACE)') -f - >/dev/null; echo $$?))
 helm_diff_is_error        := $(if $(filter-out 0 1,$(helm_diff_status)),1)
 
 helm-deploy:
 	$(if $(helm_dependencies_drifted),$(call run_step,helm-dependency-update))
-	$(if $(call kube_ns_exists,$(HELM_NAMESPACE)),,$(KUBECTL) create ns $(HELM_NAMESPACE))
+	$(if $(HELM_NAMESPACE),$(if $(call kube_ns_exists,$(HELM_NAMESPACE)),,$(KUBECTL) create ns $(HELM_NAMESPACE)))
 	$(HELM) $(HELM_DEPLOY_VERB) '$(HELM_RELEASE)' '$(HELM_CHART)' $(HELM_VALUES_ARGS)
 
 helm-dependency-update:
