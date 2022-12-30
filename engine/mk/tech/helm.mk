e6e90ec107494bc3afc343d2d0f558d91af2d415
James Lott <james.l@jarv.us>
2022-12-30 10:26:28 -0500

    engine: helm.mk: remove non-portable use of "&>"

--- engine/mk/tech/helm.mk
+++ engine/mk/tech/helm.mk
@@ -112,8 +112,7 @@ HELM_ARGS         = $(if $(HELM_NAMESPACE),--namespace '$(HELM_NAMESPACE)')
 
 HELM             += $(HELM_ARGS)
 
-# FIXME: "&>" is non-portable
-helm_release_exists        = $(shell if $(HELM) status '$(HELM_RELEASE)' &>/dev/null; then echo 1; else false; fi)
+helm_release_exists        = $(shell if $(HELM) status '$(HELM_RELEASE)' >/dev/null 2>&1; then echo 1; else false; fi)
 helm_release_changed       = $(or $(filter 1,$(helm_diff_status)),$(if $(helm_diff_is_error),$(info NOTICE: helm_diff_status encountered an error in stage $(PIPELINE_NAME)/$(STAGE_NAME) for subject $(SUBJECT_NAME); deployment will be unchanged)))
 helm_dependencies_drifted  = $(shell $(HELM) dependency list '$(HELM_CHART)' | tail -n+2 | grep -v 'ok[[:space:]]*$$' | awk '{print $$1}')
 helm_diff_status          := $(if $(helm_release_exists),$(shell $(HELM) template '$(HELM_RELEASE)' '$(HELM_CHART)' $(HELM_VALUES_ARGS) | $(KUBECTL) diff $(if $(HELM_NAMESPACE),--namespace '$(HELM_NAMESPACE)') -f - >/dev/null; echo $$?))
