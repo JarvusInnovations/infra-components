9d9c7601347d13b03de829c37fa8f2c66802e3a7
James Lott <james.l@jarv.us>
2022-12-31 22:12:01 -0500

    engine: ssh.mk: add sshConfigArtifact

--- engine/mk/tech/ssh.mk
+++ engine/mk/tech/ssh.mk
@@ -11,11 +11,16 @@
 #
 # |================================================
 # | Section       | Name              | Description
-# | engineEnv     | sshConfigPath     | Env subpath to ssh_config file
+# | engineEnv     | sshConfigPath     | Env subpath to ssh_config file. Overrides engineEnv.sshConfigArtifact.
+# | engineEnv     | sshConfigArtifact | Name of artifact pointing to a ssh_config file
 # |================================================
 
 SSH        ?= ssh
 SSH_CONFIG ?= $(call env_config_path,sshConfigPath)
+
+ifeq ($(SSH_CONFIG),)
+SSH_CONFIG := $(call env_config_artifact,sshConfigArtifact)
+endif
 
 ifneq ($(SSH_CONFIG),)
 SSH += -F '$(SSH_CONFIG)'
