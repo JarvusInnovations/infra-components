67f544126f8f04493c05bce52c672d39b8497b5f
James Lott <james.l@jarv.us>
2023-01-10 10:21:31 -0500

    engine: case.mk: override ENGINE_* context for all cases
    This ensures that individual test cases run with the correct context
    when, e.g., they are being run directly, without being passed context
    from the suite level.

--- tests/content/mk/case.mk
+++ tests/content/mk/case.mk
@@ -1,4 +1,6 @@
-ENGINE_ENV := tests
+ENGINE_PROJECT_DIR ?= $(shell realpath ../../../..)
+ENGINE_SYSTEM_DIR  ?= $(shell realpath ../../../../..)
+ENGINE_ENV         ?= tests
 include ../../../../../mk/subject.mk
 test_skip_subject = $(info SKIP: $(PIPELINE_NAME)/$(SUBJECT_NAME): $(1) not configured)
 test_skip_env     = $(info SKIP: environment $(ENGINE_ENV): $(1) not configured)
