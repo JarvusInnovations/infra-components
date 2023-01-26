4660ca3b0977cac1aadda61589b0d7ecf02d215f
James Lott <james.l@jarv.us>
2023-01-26 17:41:22 -0500

    engine: ssh.mk: refactor with opt interface

--- mk/tech/ssh.mk
+++ mk/tech/ssh.mk
@@ -1,26 +1,16 @@
-#
-# Config:
-#
-# [engineEnv "<env-name>"]
-# sshConfigPath = <env-subpath>
 # = ssh =
 #
 # OpenSSH remote management client
 #
-# == Inputs ==
+# == Options ==
 #
 # |================================================
-# | Section       | Name              | Description
-# | engineEnv     | sshConfigPath     | Env subpath to ssh_config file. Overrides engineEnv.sshConfigArtifact.
-# | engineEnv     | sshConfigArtifact | Name of artifact pointing to a ssh_config file
+# | Name          | Reference Type    | Description
+# | sshConfig     | var               | Path to ssh_config file
 # |================================================
 
 SSH        ?= ssh
-SSH_CONFIG ?= $(call env_config_path,sshConfigPath)
-
-ifeq ($(SSH_CONFIG),)
-SSH_CONFIG := $(call env_config_artifact,sshConfigArtifact)
-endif
+SSH_CONFIG ?= $(call opt_pipeline_var,sshConfig)
 
 ifneq ($(SSH_CONFIG),)
 SSH += -F '$(SSH_CONFIG)'
