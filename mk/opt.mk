e2681961bafeaa0d07b2cd1f25023d78b8f72441
James Lott <james.l@jarv.us>
2023-01-26 17:01:24 -0500

    engine: stmt-select.sh, opt.mk: rename RET_TYPE to RETURN_MODE
    The distinction between a "variable" and a "list" is one that needs to
    be made at a higher level than the statement selection interface.

--- mk/opt.mk
+++ mk/opt.mk
@@ -15,15 +15,15 @@ OPT_CONTEXT            = \
 STMT_SELECT = $(OPT_CONTEXT) $(LIB)/sh/stmt-select.sh
 
 # Methods
-opt_pipeline_var   = $(shell $(STMT_SELECT) -t pipelineOpts -c values -r variable $(foreach key,$(1),-k '$(key)') $(if $(2),-p '$(2)') $(OPTSFILE_PATHS))
-opt_pipeline_list  = $(shell $(STMT_SELECT) -t pipelineOpts -c values -r list     $(foreach key,$(1),-k '$(key)') $(OPTSFILE_PATHS))
-opt_artifact_var   = $(shell $(STMT_SELECT) -t artifactOpts -c values -r variable $(foreach aid,$(1),-a '$(aid)') $(foreach key,$(2),-k '$(key)') $(if $(3),-p '$(3)') $(OPTSFILE_PATHS))
-opt_artifact_list  = $(shell $(STMT_SELECT) -t artifactOpts -c values -r list     $(foreach aid,$(1),-a '$(aid)') $(foreach key,$(2),-k '$(key)') $(OPTSFILE_PATHS))
-artifact_path      = $(shell $(STMT_SELECT) -t artifactRefs -c values -r variable $(foreach key,$(1),-k '$(key)') $(OPTSFILE_PATHS))
-artifact_frompath  = $(shell $(STMT_SELECT) -t artifactRefs -c keys   -r variable $(foreach val,$(1),-v '$(val)') $(OPTSFILE_PATHS))
-artifacts_matching = $(shell $(STMT_SELECT) -t artifactRefs -c keys   -r list     $(foreach key,$(1),-k '$(key)') $(OPTSFILE_PATHS))
+opt_pipeline_var   = $(shell $(STMT_SELECT) -t pipelineOpts -c values -r last $(foreach key,$(1),-k '$(key)') $(if $(2),-p '$(2)') $(OPTSFILE_PATHS))
+opt_pipeline_list  = $(shell $(STMT_SELECT) -t pipelineOpts -c values -r all  $(foreach key,$(1),-k '$(key)') $(OPTSFILE_PATHS))
+opt_artifact_var   = $(shell $(STMT_SELECT) -t artifactOpts -c values -r last $(foreach aid,$(1),-a '$(aid)') $(foreach key,$(2),-k '$(key)') $(if $(3),-p '$(3)') $(OPTSFILE_PATHS))
+opt_artifact_list  = $(shell $(STMT_SELECT) -t artifactOpts -c values -r all  $(foreach aid,$(1),-a '$(aid)') $(foreach key,$(2),-k '$(key)') $(OPTSFILE_PATHS))
+artifact_path      = $(shell $(STMT_SELECT) -t artifactRefs -c values -r last $(foreach key,$(1),-k '$(key)') $(OPTSFILE_PATHS))
+artifact_frompath  = $(shell $(STMT_SELECT) -t artifactRefs -c keys   -r last $(foreach val,$(1),-v '$(val)') $(OPTSFILE_PATHS))
+artifacts_matching = $(shell $(STMT_SELECT) -t artifactRefs -c keys   -r all  $(foreach key,$(1),-k '$(key)') $(OPTSFILE_PATHS))
 
-filter_artifacts_opt_eq_val = $(sort $(basename $(shell $(STMT_SELECT) -t artifactOpts -c keys -r list $(foreach aid,$(1),-a '$(aid)') $(foreach key,$(2),-k '$(key)') $(foreach val,$(3),-v '$(val)') $(OPTSFILE_PATHS))))
+filter_artifacts_opt_eq_val = $(sort $(basename $(shell $(STMT_SELECT) -t artifactOpts -c keys -r all $(foreach aid,$(1),-a '$(aid)') $(foreach key,$(2),-k '$(key)') $(foreach val,$(3),-v '$(val)') $(OPTSFILE_PATHS))))
 filter_artifacts_opt_ne_val = $(sort $(filter-out $(call filter_artifacts_opt_eq_val,$(1),$(2),$(3)),$(1)))
 
 # Internal
