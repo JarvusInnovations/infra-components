420ff9de68c36d3d4d4512ac90f268780f066b11
James Lott <james.l@jarv.us>
2023-01-31 19:59:56 -0500

    engine: virsh.mk: refactor with opt interface

--- engine/mk/tech/virsh.mk
+++ engine/mk/tech/virsh.mk
@@ -2,13 +2,13 @@
 #
 # The libvirt CLI interface
 #
-# == Inputs ==
+# == Options ==
 #
-# |================================================
-# | Section       | Name              | Description
-# | engineSubject | virshDomain       | The name of the target domain
-# | engineSubject | virshBaseSnapshot | The name of an existing snapshot for `virshDomain`
-# |================================================
+# |======================================================
+# | Name                | Reference Type    | Description
+# | virshDomain         | var               | The name of the target domain
+# | virshBaseSnapshot   | var               | The name of an existing snapshot for `virshDomain`
+# |======================================================
 #
 # == Steps ==
 #
@@ -16,34 +16,34 @@
 #   description:::
 #     Starts the specified domain
 #   inputs:::
-#     * engineSubject.virshDomain
+#     * virshDomain
 #
 # `virsh-domain-stop`::
 #   description:::
 #     Stops the specified domain
 #   inputs:::
-#     * engineSubject.virshDomain
+#     * virshDomain
 #
 #  `virsh-domain-reset`::
 #    description:::
 #      Reverts the specified domain to the specified snapshot
 #    inputs:::
-#     * engineSubject.virshDomain
-#     * engineSubject.virshBaseSnapshot
+#     * virshDomain
+#     * virshBaseSnapshot
 #
 # == Methods ==
 #
 # `virsh_domain_running`::
 #   inputs:::
-#     * engineSubject.virshDomain
+#     * virshDomain
 #   return:::
-#     * Non-empty if the specified domain is in a "running" state
-#     * Empty if in any other state
+#     * Non-empty if `virshDomain` is in a "running" state
+#     * Empty if `virshDomain` in any other state
 
 VIRSH ?= virsh
 
-VIRSH_DOMAIN        ?= $(call subject_config,virshDomain)
-VIRSH_BASE_SNAPSHOT ?= $(call subject_config,virshBaseSnapshot)
+VIRSH_DOMAIN        ?= $(call opt_pipeline_var,virshDomain)
+VIRSH_BASE_SNAPSHOT ?= $(call opt_pipeline_var,virshBaseSnapshot)
 
 virsh_domain_running = $(filter running,$(shell $(VIRSH) domstate '$(VIRSH_DOMAIN)'))
 
