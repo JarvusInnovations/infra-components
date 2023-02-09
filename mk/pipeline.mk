71d71ae84630006e1e1099f12b40bab0c7b6ac38
James Lott <james.l@jarv.us>
2023-02-09 13:33:01 -0500

    engine: Makefile, pipeline.mk, engine.mk: refactor to implicitly create engine dirs
    Requiring a separate "init" command to be run is not necessary, and can
    easily be forgotten when a dirctory location is changed. Instead, engine
    should always ensure its required directories exist at runtime.

--- mk/pipeline.mk
+++ mk/pipeline.mk
@@ -11,8 +11,11 @@ endif
 export PIPELINE_NAME
 
 $(info STARTUP: ENGINE_ENV=$(ENGINE_ENV))
-$(info STARTUP: ENGINE_LOCAL_DIR=$(ENGINE_LOCAL_DIR))
 $(info STARTUP: ENGINE_PROJECT_DIR=$(ENGINE_PROJECT_DIR))
+$(info STARTUP: ENGINE_LOCAL_DIR=$(ENGINE_LOCAL_DIR))
+$(info STARTUP: ENGINE_ARTIFACTS_DIR=$(ENGINE_ARTIFACTS_DIR))
+$(shell mkdir -pv '$(ENGINE_LOCAL_DIR)'     >&2)
+$(shell mkdir -pv '$(ENGINE_ARTIFACTS_DIR)' >&2)
 
 run_stage = $(if $(filter $(1), $(DO_STAGES)), $(MAKE) -C $(1) stage)
 
