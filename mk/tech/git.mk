2f4d5f73c25d6ac893af367fbb3120a9dd517265
James Lott <james.l@jarv.us>
2023-03-02 18:39:43 -0500

    engine: git.mk: add git-push publisher

--- mk/tech/git.mk
+++ mk/tech/git.mk
@@ -19,12 +19,32 @@ GIT_FETCH_ARTIFACTS  := $(filter $(GIT_ARTIFACT_FILTERS),$(GIT_FETCH_ARTIFACTS))
 endif
 endif
 
+ifeq ($(GIT_PUSH_ARTIFACTS),)
+GIT_PUSH_ARTIFACTS  := $(call filter_artifacts_var_eq_val,$(call artifacts_matching,.+),publisherItem,git-push)
+GIT_PUSH_ARTIFACTS  := $(call filter_artifacts_var_eq_val,$(GIT_PUSH_ARTIFACTS),gitRef)
+ifneq ($(GIT_ARTIFACT_FILTERS),)
+GIT_PUSH_ARTIFACTS  := $(filter $(GIT_ARTIFACT_FILTERS),$(GIT_PUSH_ARTIFACTS))
+endif
+endif
+
 GIT_FETCH_ARTIFACT_TARGETS := $(patsubst %,git-fetch-artifacts-produce-%,$(GIT_FETCH_ARTIFACTS))
+GIT_PUSH_ARTIFACT_TARGETS  := $(patsubst %,git-push-artifacts-publish-%,$(GIT_PUSH_ARTIFACTS))
+
+git_force_flag = $(if $(filter-out 0 false,$(shell echo $(call opt_artifact_var,$(2),$(1)) | tr '[:upper:]' '[:lower:]')),+)
 
 git_fetch_args = \
   $(call opt_artifact_var,$(1),gitFetchRemote) \
 	$(if $(call opt_artifact_var,$(1),gitFetchRef),$(call opt_artifact_var,$(1),gitFetchRef):)$(call opt_artifact_var,$(1),gitRef)
 
+define git_push_artifact_to_remote
+$(GIT) push $(2) $(call git_force_flag,gitForcePush,$(1))$(call opt_artifact_var,$(1),gitRef)$(if $(call opt_artifact_var,$(1),gitPushRef),:$(call opt_artifact_var,$(1),gitPushRef))
+
+endef
+
 git-fetch-artifacts-produce: $(GIT_FETCH_ARTIFACT_TARGETS)
 git-fetch-artifacts-produce-%:
 	$(GIT) fetch $(call opt_artifact_var,gitFetchRemote,$*) $(call git_fetch_args,$*)
+
+git-push-artifacts-publish: $(GIT_PUSH_ARTIFACT_TARGETS)
+git-push-artifacts-publish-%:
+	$(foreach remote,$(call opt_artifact_list,$*,gitPushRemoteItem),$(call git_push_artifact_to_remote,$*,$(remote)))
