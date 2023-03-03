46fe2bc73de758d50b8d4b04e45808bd594f2932
James Lott <james.l@jarv.us>
2023-03-03 15:19:48 -0500

    engine: git.mk: add default GIT_WORKDIR

--- mk/tech/git.mk
+++ mk/tech/git.mk
@@ -67,6 +67,10 @@ ifeq ($(GIT_WORKDIR),)
 GIT_WORKDIR := $(call opt_pipeline_var,gitWorkDir)
 endif
 
+ifeq ($(GIT_WORKDIR),)
+GIT_WORKDIR := $(call path_relto,$(ENGINE_PROJECT_DIR),.)
+endif
+
 ifeq ($(GIT),)
 GIT := git $(if $(GIT_WORKDIR),-C '$(GIT_WORKDIR)')
 endif
